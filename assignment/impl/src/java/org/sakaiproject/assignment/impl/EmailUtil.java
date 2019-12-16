093f945274fe3ef6b573f817ce12729116566fa5
bbailla2 <bbailla2@uwo.ca>
2019-12-16 14:37:58 -0500

    OWL-3669 - ported OWL-3199

--- assignment/impl/src/java/org/sakaiproject/assignment/impl/EmailUtil.java
+++ assignment/impl/src/java/org/sakaiproject/assignment/impl/EmailUtil.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.sakaiproject.assignment.api.AssignmentService;
+import org.sakaiproject.assignment.api.AssignmentConstants;
 import org.sakaiproject.assignment.api.model.Assignment;
 import org.sakaiproject.assignment.api.model.AssignmentSubmission;
 import org.sakaiproject.assignment.api.model.AssignmentSubmissionSubmitter;
@@ -211,16 +212,18 @@ public class EmailUtil {
             //if this is a archive (zip etc) append the list of files in it
             attachments.stream().map(attachment -> entityManager.newReference(attachment)).forEach(reference -> {
                 ResourceProperties properties = reference.getProperties();
-                boolean isArchiveFile = isArchiveFile(reference);
-                buffer.append(properties.getProperty(ResourceProperties.PROP_DISPLAY_NAME))
-                        .append(" (")
-                        .append(reference.getProperties().getPropertyFormatted(ResourceProperties.PROP_CONTENT_LENGTH))
-                        .append(isArchiveFile ? "):" : ")")
-                        .append(NEW_LINE);
-                if (isArchiveFile(reference)) {
-                    buffer.append("<blockquote>\n");
-                    buffer.append(getArchiveManifest(reference, true));
-                    buffer.append("</blockquote>\n");
+                if (!"true".equals(properties.getProperty(AssignmentConstants.PROP_INLINE_SUBMISSION))) {
+                    boolean isArchiveFile = isArchiveFile(reference);
+                    buffer.append(properties.getProperty(ResourceProperties.PROP_DISPLAY_NAME))
+                            .append(" (")
+                            .append(reference.getProperties().getPropertyFormatted(ResourceProperties.PROP_CONTENT_LENGTH))
+                            .append(isArchiveFile ? "):" : ")")
+                            .append(NEW_LINE);
+                    if (isArchiveFile(reference)) {
+                        buffer.append("<blockquote>\n");
+                        buffer.append(getArchiveManifest(reference, true));
+                        buffer.append("</blockquote>\n");
+                    }
                 }
             });
         }
